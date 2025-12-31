ac1093169b61fc89bb0d9832fff3a87e5e1425ba
Osi <osi@tebengan.id>
2026-01-01 05:53:21 +0700

    delete transaction page

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Receipt from "./pages/Receipt";
-import Transaction from "./pages/Transaction";
 
 const queryClient = new QueryClient();
 
@@ -19,7 +18,6 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/receipt" element={<Receipt />} />
-          <Route path="/transactions" element={<Transaction />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
